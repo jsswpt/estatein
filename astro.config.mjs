a60f732d3270e2469c583968d96ace1471e05f37
jsswpt <paiusov.d@yandex.ru>
2024-10-09 21:35:33 +0500

    hotfix: fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import { resolve } from "node:path";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: { enabled: false },
   integrations: [react()],
-  vite: {
-    resolve: { alias: { root: resolve(__dirname, "./src") } },
-  },
 });
